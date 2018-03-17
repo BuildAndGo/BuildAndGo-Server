4d84610b0f566fb1ba562cced499d7fd21e34a5f
Sarah Wooten <snwooten@gmail.com>
2018-03-17 19:01:49 -0400

    Delete secrets.js

--- secrets.js
+++
@@ -1,7 +0,0 @@
-process.env.GOOGLE_ANDROID_CLIENT_ID = '342790882338-3iiukr9jkmeiqe0pfga8st7oisa1dd66.apps.googleusercontent.com';
-process.env.GOOGLE_CLIENT_SECRET = 'IEBqj7wZo5VK8-Au041UMSfl';
-process.env.GOOGLE_CALLBACK = 'auth/google/verify'
-
-process.env.FB_CLIENT_ID = '1826834400668861';
-process.env.FB_CLIENT_SECRET = '1e4c2e20ff2ad98d64d2d070d3e606fc';
-process.env.FB_CALLBACK = 'auth/fb/verify'
