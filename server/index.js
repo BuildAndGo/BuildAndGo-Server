cc0126fdc3104bd8d94016d858a46433492790e5
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-03-19 11:34:47 -0400

    initial deploy to heroku

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const compression = require('compression');
-const session = require('express-session');
-const passport = require('passport');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const db = require('./db');
-const sessionStore = new SequelizeStore({db});
-const PORT = process.env.PORT || 8080;
-const app = express();
-const socketio = require('socket.io');
-module.exports = app;
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const session = require('express-session')
+const passport = require('passport')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const db = require('./db')
+const sessionStore = new SequelizeStore({db})
+const PORT = process.env.PORT || 8080
+const app = express()
+const socketio = require('socket.io')
+module.exports = app
 
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
@@ -61,11 +61,6 @@ const createApp = () => {
     }
   })
 
-  // // sends index.html
-  // app.use('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  // })
-
   // error handling endware
   app.use((err, req, res, next) => {
     console.error(err)
@@ -83,7 +78,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync({force:false})
+const syncDb = () => db.sync()
 
 
 if (require.main === module) {
